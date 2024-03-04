ea0a7459c4affe9992cc7662a5f56016ec245f35
ntlifee <perfilov.mv@yandex.ru>
2024-03-04 17:44:38 +0300

    Рабочая игра(локально)

--- Mathematical_Carousel_Game/Backend/js/scripts.js
+++ Mathematical_Carousel_Game/Backend/js/scripts.js
@@ -2,19 +2,11 @@ var dataJson = {
     "score_first_question": 3,
     "score_failure": 3,
     "score_success": 1,
-    "total_questions": 50,
+    "total_questions": 10,
     "questions": [
-        {
-            "question": "Какой самый длинный река в мире?",
-            "answer": "Амазонка"
-        },
         {
             "question": "Кто написал 'Мастер и Маргарита'?",
             "answer": "Михаил Булгаков"
-        },
-        {
-            "question": "Что такое 'фотосинтез'?",
-            "answer": "Процесс, при котором зеленые растения используют свет, чтобы превратить углекислый газ и воду в питательные вещества"
         },
         {
             "question": "Какое животное является символом России?",
@@ -29,16 +21,8 @@ var dataJson = {
             "answer": "Испарение"
         },
         {
-            "question": "Сколько километров в миле?",
-            "answer": "1.60934"
-        },
-        {
             "question": "Как называется главный герой сказки 'Красная шапочка'?",
             "answer": "Красная Шапочка"
-        },
-        {
-            "question": "Кто изображен на самой крупной американской купюре в 100 долларов?",
-            "answer": "Бенджамин Франклин"
         },
         {
             "question": "Сколько восьмерок в пакете обычных карточных колод?",
@@ -49,20 +33,8 @@ var dataJson = {
             "answer": "Денали"
         },
         {
-            "question": "Кто написал 'Гарри Поттера'?",
-            "answer": "Дж. К. Роулинг"
-        },
-        {
-            "question": "Что такое 'демократия'?",
-            "answer": "Форма правления, при которой власть принадлежит народу или их выбранным представителям"
-        },
-        {
             "question": "Как называется столица Германии?",
             "answer": "Берлин"
-        },
-        {
-            "question": "Какое химическое вещество вырабатывается желудком для пищеварения пищи?",
-            "answer": "Желудочный сок"
         },
         {
             "question": "Сколько дней в феврале в високосном году?",
@@ -71,122 +43,11 @@ var dataJson = {
         {
             "question": "Как называется самый большой океан на Земле?",
             "answer": "Тихий"
-        },
-        {
-            "question": "Кто написал 'Отцы и дети'?",
-            "answer": "Иван Тургенев"
-        },
-        {
-            "question": "Что такое 'экосистема'?",
-            "answer": "Система взаимодействия между живыми организмами и их окружающей средой"
-        },
-        {
-            "question": "Какое наименование у самого крупного косатки?",
-            "answer": "Орка"
-        },
-        {
-            "question": "Кто написал 'Горе от ума'?",
-            "answer": "Александр Грибоедов"
-        },
-        {
-            "question": "Что такое 'тектонические плиты'?",
-            "answer": "Гигантские ломки земной коры, на которых расположены континенты и океаны"
-        },
-        {
-            "question": "Какое животное является символом Китая?",
-            "answer": "Дракон"
-        },
-        {
-            "question": "Кто изображен на картинах Сальвадора Дали?",
-            "answer": "Чаще всего он сам"
-        },
-        {
-            "question": "Что такое 'периодическая таблица'?",
-            "answer": "Таблица, на которой упорядочены химические элементы по возрастанию атомного номера"
-        },
-        {
-            "question": "Сколько сантиметров в метре?",
-            "answer": "100"
-        },
-        {
-            "question": "Какое имя у дяди Кроули в сериале 'Друзья'?",
-            "answer": "Джек"
-        },
-        {
-            "question": "Какой город является столицей Великобритании?",
-            "answer": "Лондон"
-        },
-        {
-            "question": "Как называется химический элемент с атомным номером 6?",
-            "answer": "Углерод"
-        },
-        {
-            "question": "Сколько звезд на флаге США?",
-            "answer": "50"
-        },
-        {
-            "question": "Как называется процесс превращения газа в жидкость?",
-            "answer": "Конденсация"
-        },
-        {
-            "question": "Как называется жидкость, выделяемая организмами для защиты от хищников?",
-            "answer": "Яд"
-        },
-        {
-            "question": "Какой национальный парк находится в США?",
-            "answer": "Йосемити"
-        },
-        {
-            "question": "Что такое 'перистальтика'?",
-            "answer": "Волнообразные движения мышц внутренних органов, направленные на перемещение содержимого"
-        },
-        {
-            "question": "Как называется процесс превращения сахара в энергию в организме?",
-            "answer": "Гликолиз"
-        },
-        {
-            "question": "Сколько морей омывают Африку?",
-            "answer": "Два"
-        },
-        {
-            "question": "Как называется процесс превращения камня в живую ткань?",
-            "answer": "Метаморфизм"
-        },
-        {
-            "question": "Какое имя у главного героя романа 'Преступление и наказание'?",
-            "answer": "Родион Раскольников"
-        },
-        {
-            "question": "Что такое 'биомасса'?",
-            "answer": "Масса органических веществ, производимых живыми организмами"
-        },
-        {
-            "question": "Сколько штатов в США?",
-            "answer": "50"
-        },
-        {
-            "question": "Как называется главный сателлит Земли?",
-            "answer": "Луна"
-        },
-        {
-            "question": "Кто написал 'Бойцовский клуб'?",
-            "answer": "Чак Паланик"
-        },
-        {
-            "question": "Что такое 'метеорит'?",
-            "answer": "Космическое тело, попадающее на поверхность Земли"
-        },
-        {
-            "question": "Какое наименование у самой маленькой обезьяны?",
-            "answer": "Мартышка"
-        },
-        {
-            "question": "Как называется второй по величине океан на Земле?",
-            "answer": "Атлантический"
         }
     ]
 }
 var score_first_question = dataJson.score_first_question
+var score_current_question = score_first_question
 var score_failure = dataJson.score_failure
 var score_success = dataJson.score_success
 var total_questions = dataJson.total_questions
@@ -205,31 +66,56 @@ var questions = dataJson.questions
         console.error('Ошибка загрузки файла JSON:', error);
     }); */
 
-// Получаем ссылки на строки таблицы
-var headerRow = document.getElementById('header-row')
-var emptyRow = document.getElementById('empty-row')
+// Получаем элемент тела таблицы
+var tbody = document.querySelector("#progress-table");
 
-// Создаем 100 столбцов с названиями от 1 до 100
+// Создаем 100 строк и добавляем их в таблицу
 for (var i = 1; i <= total_questions; i++) {
-    var th = document.createElement('th')
-    th.textContent = i
-    headerRow.appendChild(th)
+    // Создаем новую строку
+    var row = document.createElement("tr");
+
+    // Создаем первую ячейку с номером строки
+    var cell1 = document.createElement("th");
+    cell1.textContent = i;
+
+    // Создаем вторую ячейку с любым текстом
+    var cell2 = document.createElement("th");
+    cell2.textContent = "";
+
+    // Добавляем ячейки в строку
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+
+    // Добавляем строку в тело таблицы
+    tbody.appendChild(row);
 }
 
-// Создаем 100 пустых ячеек во второй строке
-for (var i = 1; i <= total_questions; i++) {
-    var td = document.createElement('td')
-    emptyRow.appendChild(td)
-}
-var table = document.querySelector(".progress-table")
+var table = document.querySelector("#progress-table")
 table.rows[1].cells[1].innerHTML = score_first_question
 var labelQuestion = document.getElementById('label-question')
+var inputAnswer = document.getElementById('input-answer')
+var total_score = document.getElementById('count-value')
 var idxQuestion = 0;
-console.log(idxQuestion)
-console.log('kjfgnsafdnglksahfjg;jsahgflk')
 labelQuestion.innerHTML = questions[idxQuestion].question
 document.getElementById('button-form-answer').addEventListener('click', function () {
-    labelQuestion.innerHTML = questions[idxQuestion].question
-    console.log(idxQuestion)
+    if (inputAnswer.value == questions[idxQuestion].answer) {
+        total_score.innerText = Number(total_score.innerText) + score_current_question
+        score_current_question += score_success
+    }
+    else {
+        if (score_current_question - score_failure <= score_first_question) {
+            score_current_question = score_first_question
+        }
+        else {
+            score_current_question -= score_failure
+        }
+    }
+    inputAnswer.value = ''
+    if (idxQuestion == total_questions - 1) {
+        alert("Тест закончен!")
+        return
+    }
+    table.rows[idxQuestion + 2].cells[1].innerHTML = score_current_question
     idxQuestion++;
+    labelQuestion.innerHTML = questions[idxQuestion].question
 })
