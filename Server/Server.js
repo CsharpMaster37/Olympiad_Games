c5d44e0689758b7df5113fecd992444b7ebb80d2
CsharpMaster37 <misha310303@gmail.com>
2024-03-05 18:30:52 +0300

    Создание сервера

--- Server/Server.js
+++ Server/Server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 const PORT = 3000;
 const fs = require('fs')
 const path = require('path')
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
 
     if(req.url = '/'){
-        fs.readFile('../Mathematical_Square_Game/Frontend/html/game_index.html', (err,data) =>{
+        fs.readFile('../Main_Page/Frontend/html/index.html', (err,data) =>{
             if(err){
                 console.log(err)
                 res.end();
@@ -25,4 +25,40 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log('listening port ${PORT}')
+}) */
+
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const path = require('path')
+
+const createPath = (page) => path.resolve(__dirname, '../', `${page}.html`)
+
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
+
+app.use(express.static('../Mathematical_Carousel_Game/Frontend'))
+app.use(express.static('../Mathematical_Square_Game/Frontend'))
+app.use(express.static('../'))
+
+app.get('/', (req, res) => {
+    res.sendFile(createPath('Main_Page/Frontend/html/index'))
+})
+
+app.get('/square', (req, res) => {
+    res.sendFile(createPath('Mathematical_Square_Game/Frontend/html/game_index'))
+})
+
+app.get('/carousel', (req, res) => {
+    res.sendFile(createPath('Mathematical_Carousel_Game/Frontend/html/index_MathematicalCarousel'))
+})
+
+
+
+
+/* app.use((req, res) => {
+    res
+        .status(404)
+}) */
