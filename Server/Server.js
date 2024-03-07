162a33ad6f53a17b6bfc7dfec742f030ba3bc6e5
CsharpMaster37 <misha310303@gmail.com>
2024-03-07 10:24:12 +0300

    Начало подключения БД

--- Server/Server.js
+++ Server/Server.js
@@ -1,32 +1,3 @@
-/* const http = require('http');
-const PORT = 3000;
-const fs = require('fs')
-const path = require('path')
-const createPath = (page) => path.resolve(__dirname, 'views', '${page}.html')
-
-const server = http.createServer((req, res) => {
-    console.log('Server request')
-    
-    res.setHeader('Content-Type', 'text/html')
-
-    if(req.url = '/'){
-        fs.readFile('../Main_Page/Frontend/html/index.html', (err,data) =>{
-            if(err){
-                console.log(err)
-                res.end();
-            }
-            else{
-                res.write(data)
-                res.end();
-            }
-        })
-    }
-});
-
-server.listen(PORT, 'localhost', (error) => {
-    error ? console.log(error) : console.log('listening port ${PORT}')
-}) */
-
 const express = require('express')
 const app = express()
 const PORT = 3000
