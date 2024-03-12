d746e8cd4562ef0c5c6c969edab670b01c13b073
ntlifee <perfilov.mv@yandex.ru>
2024-03-12 16:21:37 +0300

    Применение шаблонизатора

--- Server/Server.js
+++ Server/Server.js
@@ -5,7 +5,7 @@ const User = require('../Database/db')
 
 const path = require('path')
 
-const createPath = (page) => path.resolve(__dirname, '../', `${page}.html`)
+const createPath = (page) => path.resolve(__dirname, '../', `${page}.ejs`)
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`)
@@ -16,27 +16,27 @@ app.use(express.static('../Mathematical_Square_Game/Frontend'))
 app.use(express.static('../Registration_Page/Frontend'))
 app.use(express.static('../'))
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('Main_Page/Frontend/html/index'))
+    res.render(createPath('Main_Page/Frontend/html/index'))
 })
 
 app.get('/square', (req, res) => {
-    res.sendFile(createPath('Mathematical_Square_Game/Frontend/html/game_index'))
+    res.render(createPath('Mathematical_Square_Game/Frontend/html/game_index'))
 })
 
 app.get('/carousel', (req, res) => {
-    res.sendFile(createPath('Mathematical_Carousel_Game/Frontend/html/index_MathematicalCarousel'))
+    res.render(createPath('Mathematical_Carousel_Game/Frontend/html/index_MathematicalCarousel'))
 })
 
 app.get('/signup', (req, res) => {
-    res.sendFile(createPath('Registration_Page/Frontend/html/registration'))
+    res.render(createPath('Registration_Page/Frontend/html/registration'))
 })
 
 app.post('/signup', (req, res) => {
-    const {username, password, repeatpassword} = req.body
-    const user = new User({username, password})
+    const { username, password, repeatpassword } = req.body
+    const user = new User({ username, password })
     user
         .save()
         .then((result) => res.send(result))
