b2a26e9df7cdfb7748813eb3f6aeaf0c5b7fae78
ntlifee <perfilov.mv@yandex.ru>
2024-03-29 16:51:37 +0300

    Доработка дизайна карусели

--- Frontend/scripts/carousel-scripts.js
+++ Frontend/scripts/carousel-scripts.js
@@ -1,4 +1,4 @@
-var dataJson = {
+/* var dataJson = {
     "score_first_question": 3,
     "score_failure": 3,
     "score_success": 1,
@@ -45,7 +45,209 @@ var dataJson = {
             "answer": "Тихий"
         }
     ]
-}
+} */
+
+var dataJson = {
+    "score_first_question": 3,
+    "score_failure": 3,
+    "score_success": 1,
+    "total_questions": 50,
+    "questions": [
+        {
+            "question": "Кто написал 'Мастер и Маргарита'?",
+            "answer": "Михаил Булгаков"
+        },
+        {
+            "question": "Какое животное является символом России?",
+            "answer": "Медведь"
+        },
+        {
+            "question": "Сколько континентов на Земле?",
+            "answer": "Семь"
+        },
+        {
+            "question": "Как называется процесс превращения воды в пар?",
+            "answer": "Испарение"
+        },
+        {
+            "question": "Как называется главный герой сказки 'Красная шапочка'?",
+            "answer": "Красная Шапочка"
+        },
+        {
+            "question": "Сколько восьмерок в пакете обычных карточных колод?",
+            "answer": "Четыре"
+        },
+        {
+            "question": "Как называется главная гора в США?",
+            "answer": "Денали"
+        },
+        {
+            "question": "Как называется столица Германии?",
+            "answer": "Берлин"
+        },
+        {
+            "question": "Сколько дней в феврале в високосном году?",
+            "answer": "29"
+        },
+        {
+            "question": "Как называется самый большой океан на Земле?",
+            "answer": "Тихий"
+        },
+        {
+            "question": "Кто написал 'Война и мир'?",
+            "answer": "Лев Толстой"
+        },
+        {
+            "question": "Как называется столица Франции?",
+            "answer": "Париж"
+        },
+        {
+            "question": "Сколько суток в високосном году?",
+            "answer": "366"
+        },
+        {
+            "question": "Кто изображен на американской сотке?",
+            "answer": "Бенджамин Франклин"
+        },
+        {
+            "question": "Какой химический элемент соответствует символу 'Fe'?",
+            "answer": "Железо"
+        },
+        {
+            "question": "Что такое 'Крокодил Гена' по профессии?",
+            "answer": "Уборщик"
+        },
+        {
+            "question": "Сколько зубов у взрослого человека?",
+            "answer": "32"
+        },
+        {
+            "question": "Как называется столица Великобритании?",
+            "answer": "Лондон"
+        },
+        {
+            "question": "Сколько планет в солнечной системе?",
+            "answer": "Восемь"
+        },
+        {
+            "question": "Как называется самая высокая гора в мире?",
+            "answer": "Эверест"
+        },
+        {
+            "question": "Кто написал 'Гарри Поттера'?",
+            "answer": "Джоан Роулинг"
+        },
+        {
+            "question": "Сколько нот в музыкальной гамме?",
+            "answer": "Семь"
+        },
+        {
+            "question": "Какое самое длинное река в мире?",
+            "answer": "Амазонка"
+        },
+        {
+            "question": "Кто изобрел телефон?",
+            "answer": "Александр Грахам Белл"
+        },
+        {
+            "question": "Как называется жидкость, которая приводит в движение двигатель автомобиля?",
+            "answer": "Бензин"
+        },
+        {
+            "question": "Какой месяц июль по счету?",
+            "answer": "Седьмой"
+        },
+        {
+            "question": "Что изучает дендролог?",
+            "answer": "Деревья"
+        },
+        {
+            "question": "Как называется столица Италии?",
+            "answer": "Рим"
+        },
+        {
+            "question": "Сколько букв в русском алфавите?",
+            "answer": "33"
+        },
+        {
+            "question": "Кто изображен на логотипе компании 'Apple'?",
+            "answer": "Яблоко"
+        },
+        {
+            "question": "Что измеряется в амперах?",
+            "answer": "Сила тока"
+        },
+        {
+            "question": "Как называется процесс кипения воды?",
+            "answer": "Кипение"
+        },
+        {
+            "question": "Сколько глаз у человека?",
+            "answer": "Два"
+        },
+        {
+            "question": "Как называется место, где держат мороженое?",
+            "answer": "Морозильник"
+        },
+        {
+            "question": "Сколько ног у паука?",
+            "answer": "Восемь"
+        },
+        {
+            "question": "Какой элемент химической таблицы обозначается как 'Na'?",
+            "answer": "Натрий"
+        },
+        {
+            "question": "Какой месяц первый в году?",
+            "answer": "Январь"
+        },
+        {
+            "question": "Сколько белых клеток в крови?",
+            "answer": "Миллионы"
+        },
+        {
+            "question": "Какой год високосный?",
+            "answer": "Каждый четвёртый"
+        },
+        {
+            "question": "Кто написал 'Повести Белкина'?",
+            "answer": "Александр Пушкин"
+        },
+        {
+            "question": "Как называется главный ингредиент в пицце?",
+            "answer": "Тесто"
+        },
+        {
+            "question": "Что измеряется в килограммах?",
+            "answer": "Масса"
+        },
+        {
+            "question": "Сколько цветов в радуге?",
+            "answer": "Семь"
+        },
+        {
+            "question": "Как называется четвертая планета от Солнца?",
+            "answer": "Марс"
+        },
+        {
+            "question": "Как называется наука о числах?",
+            "answer": "Математика"
+        },
+        {
+            "question": "Сколько игроков в полевом хоккейной команде?",
+            "answer": "Шесть"
+        },
+        {
+            "question": "Как называется жидкость, которая протекает из раны?",
+            "answer": "Кровь"
+        },
+        {
+            "question": "Как называется твердое топливо, используемое для рисования на доске?",
+            "answer": "Мел"
+        }
+    ]
+};
+
 var score_first_question = dataJson.score_first_question
 var score_current_question = score_first_question
 var score_failure = dataJson.score_failure
@@ -94,7 +296,7 @@ var table = document.querySelector("#progress-table")
 table.rows[1].cells[1].innerHTML = score_first_question
 var labelQuestion = document.getElementById('label-question')
 var inputAnswer = document.getElementById('input-answer')
-var total_score = document.getElementById('count-value')
+var total_score = document.getElementById('count-point')
 var idxQuestion = 0;
 labelQuestion.innerHTML = questions[idxQuestion].question
 document.getElementById('button-form-answer').addEventListener('click', function () {
@@ -118,6 +320,24 @@ document.getElementById('button-form-answer').addEventListener('click', function
         return
     }
     table.rows[idxQuestion + 2].cells[1].innerHTML = score_current_question
+    if (idxQuestion % 8 == 0) {
+        // Вычисляем высоту окна браузера
+        var windowHeight = window.innerHeight;
+
+        // Получаем высоту таблицы
+        var tableHeight = document.querySelector("#progress-table").offsetHeight;
+
+        // Вычисляем индекс вопроса, который должен оставаться в центре экрана
+        var centerIndex = Math.floor(idxQuestion / 2);
+
+        // Вычисляем высоту прокрутки, чтобы центрировать текущий вопрос
+        var scrollHeight = (centerIndex * 2 * tableHeight / total_questions) - (windowHeight / 2) + 150;
+
+        // Прокручиваем страницу к вычисленной высоте прокрутки
+        window.scrollTo(0, scrollHeight);
+    }
+
+
     idxQuestion++;
     labelQuestion.innerHTML = questions[idxQuestion].question
 })
