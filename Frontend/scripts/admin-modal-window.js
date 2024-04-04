ff15ee0785c9eb0c7d09b3cd7b110e4565af006c
CsharpMaster37 <misha310303@gmail.com>
2024-04-04 22:51:21 +0300

    Обновление админ панели

---
+++ Frontend/scripts/admin-modal-window.js
@@ -0,0 +1,19 @@
+function openModal_carousel(){
+    document.getElementById("carousel-admin-modal").classList.add("open");
+}
+
+function openModal_square(){
+    document.getElementById("square-admin-modal").classList.add("open");
+}
+
+document.getElementById("carousel-button-close").addEventListener("click",
+    function () {
+        document.getElementById("carousel-admin-modal").classList.remove("open")
+    }
+)
+
+document.getElementById("square-button-close").addEventListener("click",
+    function () {
+        document.getElementById("square-admin-modal").classList.remove("open")
+    }
+)
